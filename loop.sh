df6e62554ef970c27a7764280be7a468294cc753
unknown <github@lattis.se>
2015-09-02 13:37:49 +0200

    comments

---
+++ loop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Script to run a constant check for internet access
+# Should be run as sudo if you want to interface with unicornhat
+
+# First lets establish that we have an IP adress
+
+# Then lets establish that we have connectivity to the net
