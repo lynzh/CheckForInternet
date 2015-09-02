202e22b9564e4aa095715ef96cfe04ffdde84ca4
lynzh <github@lattis.se>
2015-09-02 14:39:47 +0200

    checks for connectivity

--- loop.sh
+++ loop.sh
@@ -2,6 +2,18 @@
 # Script to run a constant check for internet access
 # Should be run as sudo if you want to interface with unicornhat
 
-# First lets establish that we have an IP adress
 
-# Then lets establish that we have connectivity to the net
+PING_CHECK=$(ping -c 1 -i 1 195.159.0.100 | awk '/1\ received/ { system("echo "$4) }')
+
+RunCheck() { 
+	
+	if [ $PING_CHECK  = "1" ] ;
+	then
+		echo "## PARTY -- Connection UP ##";
+	elif [ $PING_CHECK ! "1" ];
+	then
+		echo "## Error -- Connection DOWN ##"; 
+	fi
+}
+
+RunCheck	
