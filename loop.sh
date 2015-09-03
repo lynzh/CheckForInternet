6681bc3b595f8fda9ed7ec51c41064160188a419
lynzh <github@lattis.se>
2015-09-03 15:45:52 +0200

    rewriting netcheck with 3 tests

--- loop.sh
+++ loop.sh
@@ -1,45 +1,44 @@
 #!/bin/sh
 # Script to run a constant check for internet access
 
-# initial echos
+# Initialize our VARs
+DNS1="195.159.0.100"
+DNS2="195.159.0.100"
+DNS3="4.4.4.4"
 
-if [ "$@" ] ; then
-	while sleep "$@";
-	do
-	clear;
-	echo "## Terminate with ^C or Ctrl+c ## "
+# Initialize our checks/pings
+PING_DNS1=$(ping -c 1 -i 1 -t 10 ${DNS1} | awk '/received/ { system("echo "$4) }')
+PING_DNS2=$(ping -c 1 -i 1 -t 10 ${DNS2} | awk '/received/ { system("echo "$4) }')
+PING_DNS3=$(ping -c 1 -i 1 -t 10 ${DNS3} | awk '/received/ { system("echo "$4) }')
 
-PING_CHECK=$(ping -c 1 -i 1 -t 10 195.159.0.100 | awk '/received/ { system("echo "$4) }')
-RunCheck() { 
-	
-	if [ $PING_CHECK  = "1" ] ;
-	then
-		echo "## PARTY -- Connection UP ##";
-	elif [ $PING_CHECK ! "1" ];
-	then
-		echo "## Error -- Connection DOWN ##"; 
-	fi
+#elif [ ! "$@" ] ; then
+#	echo "Error, usage --"
+#	echo "$0 <num>"
+#	echo "eg; $0 0.5"
+#	exit 1;
+#fi
+
+
+# Tests to be removed later
+
+#
+echo "\$DNS1 = $DNS1"
+echo "\$DNS1 = $DNS1"
+echo "\$DNS1 = $DNS1"
+
+echo "\$PING_DNS1 = $PING_DNS1"
+echo "\$PING_DNS2 = $PING_DNS2"
+echo "\$PING_DNS3 = $PING_DNS3"
+
+
+RunCheck() { 	
+if [ $PING_DNS1  = "1" ] ;
+then
+	exit 0	
+elif [ $PING_DNS1 ! "1" ];
+then
+	exit 1
+fi
 }
 RunCheck	
 
-PING_TEST=$(ping -c 1 -i 1 -t 10 123.123.123.142 | awk '/received/ { system("echo "$4) }')
-Runtest() { 
-	
-	if [ $PING_TEST  = "1" ] ;
-	then
-		echo "## TEST -- Connection UP ##";
-	elif [ $PING_TEST = "0" ];
-	then
-		echo "## TEST -- Connection DOWN ##"; 
-	fi
-}
-
-Runtest
-	done
-
-elif [ ! "$@" ] ; then
-	echo "Error, usage --"
-	echo "$0 <num>"
-	echo "eg; $0 0.5"
-	exit 1;
-fi	
