9bb23cb5bcebeb18bb36a7c110854102de9dbcfa
lynzh <github@lattis.se>
2015-09-04 16:08:51 +0200

    triple reduced the processing power required~~

--- loop.sh
+++ loop.sh
@@ -7,9 +7,8 @@ DNS2="178.62.149.188"
 DNS3="8.8.8.8"
 
 # Initialize our checks/pings
-PING_DNS1=$(ping -c 1 -w 1 ${DNS1} | awk '/received/ { system("echo "$4) }')
-PING_DNS2=$(ping -c 1 -w 1 ${DNS2} | awk '/received/ { system("echo "$4) }')
-PING_DNS3=$(ping -c 1 -w 1 ${DNS3} | awk '/received/ { system("echo "$4) }')
+PING_DNS1=$(ping -c 1 -w 1 ${DNS1} | \
+awk '/received/ { system("echo "$4) }')
 
 RunCheck() { 	
 if [ $PING_DNS1  = "1" ];
@@ -17,11 +16,15 @@ then
 	exit 0 # if connectivity exists to $DNS1, exit with no errors	
 elif [ $PING_DNS1 ! "1" ];
 then
+	PING_DNS2=$(ping -c 1 -w 1 ${DNS2} | \
+	awk '/received/ { system("echo "$4) }')
 	if [ $PING_DNS2  = "1" ];
 	then
 		exit 0 # OR if DNS1 fails, check for DNS2	
 	elif [ $PING_DNS2 ! "1" ];
 	then
+		PING_DNS3=$(ping -c 1 -w 1 ${DNS3} | \
+		awk '/received/ { system("echo "$4) }')
 		if [ $PING_DNS3  = "1" ];
 		then
 			exit 0 # if attempts at connctivity fails here, all the internet
