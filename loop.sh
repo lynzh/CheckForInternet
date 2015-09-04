6931a5adead2cb8ef2633318cb6ec10f2301b389
lynzh <github@lattis.se>
2015-09-04 15:32:25 +0200

    cleaned up script

--- loop.sh
+++ loop.sh
@@ -11,40 +11,21 @@ PING_DNS1=$(ping -c 1 -w 1 ${DNS1} | awk '/received/ { system("echo "$4) }')
 PING_DNS2=$(ping -c 1 -w 1 ${DNS2} | awk '/received/ { system("echo "$4) }')
 PING_DNS3=$(ping -c 1 -w 1 ${DNS3} | awk '/received/ { system("echo "$4) }')
 
-#elif [ ! "$@" ] ; then
-#	echo "Error, usage --"
-#	echo "$0 <num>"
-#	echo "eg; $0 0.5"
-#	exit 1;
-#fi
-
-
-# Tests to be removed later
-
-#
-echo "\$DNS1 = $DNS1"
-echo "\$DNS2 = $DNS2"
-echo "\$DNS3 = $DNS3"
-
-echo "\$PING_DNS1 = $PING_DNS1"
-echo "\$PING_DNS2 = $PING_DNS2"
-echo "\$PING_DNS3 = $PING_DNS3"
-
-
 RunCheck() { 	
-if [ $PING_DNS1  = "1" ] ;
+if [ $PING_DNS1  = "1" ];
 then
-	exit 0	
+	exit 0 # if connectivity exists to $DNS1, exit with no errors	
 elif [ $PING_DNS1 ! "1" ];
 then
-	if [ $PING_DNS2  = "1" ] ;
+	if [ $PING_DNS2  = "1" ];
 	then
-		exit 0	
+		exit 0 # OR if DNS1 fails, check for DNS2	
 	elif [ $PING_DNS2 ! "1" ];
 	then
-		if [ $PING_DNS3  = "1" ] ;
+		if [ $PING_DNS3  = "1" ];
 		then
-			exit 0	
+			exit 0 # if attempts at connctivity fails here, all the internet
+						 # is down OR something is wrong with our network
 		elif [ $PING_DNS3 ! "1" ];
 		then
 			exit 1
@@ -52,5 +33,7 @@ then
 	fi
 fi
 }
-RunCheck	
+
+# Runs RunCheck()
+RunCheck 
 
